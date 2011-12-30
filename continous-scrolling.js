770d2e25e0aa51b4048d0d17353990dea988374f
Ruben Jenster <ruben@ocotec.de>
2011-12-30 22:03:29 +0100

    - fix chunk index off-by-one - improve chunk loading logic, and chunk container size calculation - dynamically generate chunk containers in test

--- continous-scrolling.js
+++ continous-scrolling.js
@@ -10,7 +10,7 @@ $(".scroll-container").bind("scroll", function(){preloadElements(this);});
 // calculate the chunk heights
 var initialChunkCollapsedMargins = (initialItemsToLoad - 1) * itemMargin
 var initialChunkHeight = (initialItemsToLoad * itemHeight) - initialChunkCollapsedMargins;
-var chunkCollapsedMargins = (itemsPerChunk -1) * itemMargin;
+var chunkCollapsedMargins = (itemsPerChunk + 1) * itemMargin;
 var chunkHeight = (itemsPerChunk * itemHeight) - chunkCollapsedMargins; 
 var initialChunkOffset = initialChunkHeight - chunkHeight;
 
@@ -24,11 +24,22 @@ function preloadElements(scrollContainer) {
 	var isLoading = _.indexOf(loadingChunks, chunkIndex) > -1;
 	var isLoaded = _.indexOf(loadedChunks, chunkIndex) > -1;
 	
-	if (!isLoading && !isLoaded) {
+	if (isChunkReadyToLoad(chunkIndex)) {
 		loadingChunks.push(chunkIndex);
 		setTimeout(function() {
-				if (Math.abs(chunkIndex - getChunkIndex(scrollContainer)) < 2) {
+				if ((chunkIndex - getChunkIndex(scrollContainer)) == 0) {
 					// for testing purposes, use setTimeout to simulate HTTP transmission delay
+					
+					// TODO load previous,current and next chunk at once, use smaller chunks?
+					var previousChunk = chunkIndex -1;
+					var nextChunk = chunkIndex + 1;
+					if (isChunkReadyToLoad(previousChunk)) {
+						setTimeout(function(){ loadChunk(previousChunk, itemsPerChunk); }, 500);
+					}
+					if (isChunkReadyToLoad(nextChunk)) {
+						setTimeout(function(){ loadChunk(nextChunk, itemsPerChunk); }, 500);
+					}
+
 					setTimeout(function(){ loadChunk(chunkIndex, itemsPerChunk); }, 500);
 				} else {
 					// remove from loading chunks again
@@ -39,17 +50,24 @@ function preloadElements(scrollContainer) {
 	}
 }
 
+function isChunkReadyToLoad(chunkIndex) {
+	var validIndex = chunkIndex > -1;
+	var isLoading = _.indexOf(loadingChunks, chunkIndex) > -1;
+	var isLoaded = _.indexOf(loadedChunks, chunkIndex) > -1;
+	return validIndex && !isLoading && !isLoaded;
+}
+
 function getChunkIndex(scrollContainer) {
 	// http://www.quirksmode.org/dom/w3c_cssom.html#elementview, content field height including padding
 	//var scrollContainerHeight = scrollContainer.clientHeight;
 
 	var scrollBarPosition = scrollContainer.scrollTop; // position of scrollbar
-	//console.log("scrollbar position: " + scrollBarPosition);
+	console.log("scrollbar position: " + scrollBarPosition);
 
 	// increment active chunk by one since the first chunk is initially loaded
 	// remove the offset of the first chunk
 	// toFixed returns a string, therefore we have to call parseInt to make to calculation work
-	return 1 + parseInt(((scrollBarPosition - initialChunkOffset) / chunkHeight).toFixed(0));
+	return parseInt(((scrollBarPosition - initialChunkOffset) / chunkHeight).toFixed(0));
 }
 
 function loadChunk(chunkIndex, itemCount) {
@@ -59,7 +77,7 @@ function loadChunk(chunkIndex, itemCount) {
 	var chunkContainer = $(chunkContainerSelector);
 	
 	// append the callback return value to the chunk container
-	chunkContainer.append(loadChunkContent(chunkIndex, itemCount));
+	chunkContainer.replaceWith(loadChunkContent(chunkIndex, itemCount));
 
 	// mark the chunk as loaded
 	loadedChunks.push(chunkIndex);
@@ -68,6 +86,6 @@ function loadChunk(chunkIndex, itemCount) {
 }
 
 function loadChunkContent(chunkIndex, items) {
-	var itemTemplate = '<% _.each(items, function(item) { %> <li class="scroll-item">chunk <%= chunkIndex %>.<%= item %></li> <% }); %>';
+	var itemTemplate = '<% _.each(items, function(item) { %> <li>chunk <%= chunkIndex %>.<%= item %></li> <% }); %>';
 	return _.template(itemTemplate, {'items' : _.range(items), 'chunkIndex' : chunkIndex});
 }
