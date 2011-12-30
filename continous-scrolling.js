18c89a1f1372a8cc78381df9198434220cc51111
Ruben Jenster <ruben@ocotec.de>
2011-12-30 21:07:22 +0100

    dump load timeout implementation

--- continous-scrolling.js
+++ continous-scrolling.js
@@ -18,24 +18,38 @@ var loadedChunks = []; // the chunks already loaded
 var loadingChunks = []; // the chunks currently loading
 
 function preloadElements(scrollContainer) {
-	
-	//var scrollContainerHeight = scrollContainer.clientHeight; // http://www.quirksmode.org/dom/w3c_cssom.html#elementview, content field height including padding
-	var scrollBarPosition = scrollContainer.scrollTop; // position of scrollbar
-
-	// increment active chunk by one since the first chunk is initially loaded
-	// remove the offset of the first chunk
-	// toFixed returns a string, therefore we have to call parseInt to make to calculation work
-	var chunkIndex = 1 + parseInt(((scrollBarPosition - initialChunkOffset) / chunkHeight).toFixed(0));
+	var chunkIndex = getChunkIndex(scrollContainer);
+	console.log("chunk " + chunkIndex); 
 	
 	var isLoading = _.indexOf(loadingChunks, chunkIndex) > -1;
 	var isLoaded = _.indexOf(loadedChunks, chunkIndex) > -1;
 	
 	if (!isLoading && !isLoaded) {
 		loadingChunks.push(chunkIndex);
-		console.log("scrollbar position: " + scrollBarPosition);
-		// for testing purposes, use setTimeout to simulate HTTP transmission delay
-		setTimeout(function(){ loadChunk(chunkIndex, itemsPerChunk); }, 500);
+		setTimeout(function() {
+				if (Math.abs(chunkIndex - getChunkIndex(scrollContainer)) < 2) {
+					// for testing purposes, use setTimeout to simulate HTTP transmission delay
+					setTimeout(function(){ loadChunk(chunkIndex, itemsPerChunk); }, 500);
+				} else {
+					// remove from loading chunks again
+					loadingChunks = _.without(loadingChunks, chunkIndex);
+					console.log("loading timeout for chunk: " + chunkIndex);
+				}
+			}, loadDelay);
 	}
+}
+
+function getChunkIndex(scrollContainer) {
+	// http://www.quirksmode.org/dom/w3c_cssom.html#elementview, content field height including padding
+	//var scrollContainerHeight = scrollContainer.clientHeight;
+
+	var scrollBarPosition = scrollContainer.scrollTop; // position of scrollbar
+	//console.log("scrollbar position: " + scrollBarPosition);
+
+	// increment active chunk by one since the first chunk is initially loaded
+	// remove the offset of the first chunk
+	// toFixed returns a string, therefore we have to call parseInt to make to calculation work
+	return 1 + parseInt(((scrollBarPosition - initialChunkOffset) / chunkHeight).toFixed(0));
 }
 
 function loadChunk(chunkIndex, itemCount) {
